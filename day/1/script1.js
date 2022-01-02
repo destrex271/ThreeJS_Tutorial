5b73ad743de1c174bb4a03c7036901cd6e7c4901
destrex271 <destrex271@gmail.com>
2022-01-02 20:39:32 +0530

    5th commit

--- day/1/script1.js
+++ day/1/script1.js
@@ -1,19 +1,20 @@
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({material: 0xffffff})
+const material = new THREE.MeshBasicMaterial({color:0x0000ffaa})
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.postion.z = 5
-
+camera.position.z = 5
 
 function animate(){
     requestAnimationFrame(animate)
+    cube.rotation.x += 0.1
+    cube.rotation.y += 0.1
     renderer.render(scene, camera)
 }
 
