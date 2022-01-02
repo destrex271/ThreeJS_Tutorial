e173e24da489c7646971a4d11a5b7317be86a2e2
destrex271 <destrex271@gmail.com>
2022-01-02 20:30:12 +0530

    3rdd commit

--- day/1/script1.js
+++ day/1/script1.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer)
+document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({material: 0x0000ff})
