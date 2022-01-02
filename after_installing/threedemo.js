5375f70c90bb829c5bacb69ffff38f8d684caf09
destrex271 <destrex271@gmail.com>
2022-01-02 21:08:27 +0530

    7th commit

---
+++ after_installing/threedemo.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+
+
+import { Scene } from 'three';
+
+const scene = new Scene()
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+const controls = new OrbitControls();
+
